a3f2991c48ec0295e85d00efc506a177685b8033
thanhhung87 <thanhhung1.vip@gmail.com>
2026-06-11 09:26:09 +0700

    refactor: remove React Query and BrowserRouter providers from main entry point

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -1,17 +1,9 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { App } from '@/App';
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
